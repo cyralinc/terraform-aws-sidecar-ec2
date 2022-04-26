34a897c419afb4f1e481daa4746019738525fbb6
yowgf <alexthomasmol@gmail.com>
2022-04-26 16:05:40 -0300

    Let application decide default ss cert host

--- lambda_resources.tf
+++ lambda_resources.tf
@@ -1,11 +1,3 @@
-locals {
-  sidecar_created_certificate_sidecar_hosts = "${var.sidecar_dns_name}" != "" ? (
-    "${var.sidecar_dns_name}"
-    ) : (
-    "sidecar.cyral.com"
-  )
-}
-
 resource "aws_lambda_function" "sidecar_created_certificate" {
   function_name = "${var.name_prefix}-sidecar_created_certificate"
   role          = aws_iam_role.sidecar_created_certificate_lambda_execution.arn
@@ -19,7 +11,7 @@ resource "aws_lambda_function" "sidecar_created_certificate" {
     variables = {
       SIDECAR_CREATED_CERTIFICATE_AWS_REGION    = data.aws_arn.cw_lg.region
       SIDECAR_CREATED_CERTIFICATE_SIDECAR_ID    = var.sidecar_id
-      SIDECAR_CREATED_CERTIFICATE_SIDECAR_HOSTS = local.sidecar_created_certificate_sidecar_hosts
+      SIDECAR_CREATED_CERTIFICATE_SIDECAR_HOSTS = var.sidecar_dns_name
     }
   }
 }
