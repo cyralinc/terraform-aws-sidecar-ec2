4e339f7b73855e2e94798c3b8da162a1c2928f09
Antônio Franco <13881523+antoniomrfranco@users.noreply.github.com>
2023-04-21 12:46:04 -0300

    move secrets to secrets manager

--- secretsmanager_resources.tf
+++ secretsmanager_resources.tf
@@ -1,8 +1,10 @@
 locals {
   sidecar_secrets = {
-    clientId             = var.client_id
-    clientSecret         = var.client_secret
-    containerRegistryKey = var.container_registry_key
+    clientId                    = var.client_id
+    clientSecret                = var.client_secret
+    containerRegistryKey        = var.container_registry_key
+    sidecarPublicIdpCertificate = var.sidecar_public_idp_certificate
+    sidecarPrivateIdpKey        = var.sidecar_private_idp_key
   }
   create_sidecar_custom_certificate_secret = var.sidecar_custom_certificate_account_id != ""
 }
