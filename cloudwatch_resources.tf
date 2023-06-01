f0bcf20d8d5b8c94a71781df9f69df57bf47ae34
gabriel <gabpaivapereira@gmail.com>
2023-06-01 18:48:28 -0300

    Add external parameter to name cloudwatch log groups

--- cloudwatch_resources.tf
+++ cloudwatch_resources.tf
@@ -1,4 +1,4 @@
 resource "aws_cloudwatch_log_group" "cyral-sidecar-lg" {
-  name              = local.name_prefix
+  name              = local.cloudwatch_log_group_name
   retention_in_days = var.cloudwatch_logs_retention
 }
