7e07f0a230d3b8e6a270fc219d6bee9eb1aefa40
Antônio Franco <13881523+antoniomrfranco@users.noreply.github.com>
2024-09-09 18:19:25 -0300

    Fix tag_specifications for_each

--- ec2_resources.tf
+++ ec2_resources.tf
@@ -64,7 +64,7 @@ EOT
   }
 
   dynamic "tag_specifications" {
-    for_each = var.custom_tags != {} ? var.launch_template_tags_resource_types : []
+    for_each = length(var.custom_tags) > 0 ? var.launch_template_tags_resource_types : []
     content {
       resource_type = tag_specifications.key
       tags          = var.custom_tags
