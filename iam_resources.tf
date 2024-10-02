6cfb18af1e7e23d67d04dad770ec922be17dc0a5
Wilson de Carvalho <796900+wcmjunior@users.noreply.github.com>
2024-10-02 12:32:09 -0700

    Fix deprecation message

--- iam_resources.tf
+++ iam_resources.tf
@@ -225,32 +225,41 @@ resource "aws_iam_role" "self_signed_certificate" {
       },
     ]
   })
-  inline_policy {
-    name = "CertificateManagerLambdaPolicy"
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Effect = "Allow"
-          Action = [
-            "logs:CreateLogGroup",
-            "logs:CreateLogStream",
-            "logs:PutLogEvents",
-          ]
-          Resource = "arn:${local.aws_partition}:logs:${local.aws_region}:${local.aws_account_id}:*"
-        },
-        {
-          Effect = "Allow"
-          Action = [
-            "secretsmanager:GetSecretValue",
-            "secretsmanager:UpdateSecret",
-          ]
-          Resource = [
-            "arn:${local.aws_partition}:secretsmanager:${local.aws_region}:${local.aws_account_id}:secret:/cyral/sidecars/${var.sidecar_id}/self-signed-certificate*",
-            "arn:${local.aws_partition}:secretsmanager:${local.aws_region}:${local.aws_account_id}:secret:/cyral/sidecars/${var.sidecar_id}/ca-certificate*",
-          ]
-        },
-      ]
-    })
+}
+
+resource "aws_iam_policy" "self_signed_certificate" {
+  name        = "${local.name_prefix}-self_signed_certificate"
+  path        = "/"
+  description = "Allow lambda to create a self-signed certificate"
+  policy      = data.aws_iam_policy_document.self_signed_certificate.json
+  tags        = var.custom_tags
+}
+
+data "aws_iam_policy_document" "self_signed_certificate" {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+    resources = [
+      "arn:${local.aws_partition}:logs:${local.aws_region}:${local.aws_account_id}:*"
+    ]
   }
+
+  statement {
+    actions = [
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:UpdateSecret",
+    ]
+    resources = [
+      "arn:${local.aws_partition}:secretsmanager:${local.aws_region}:${local.aws_account_id}:secret:/cyral/sidecars/${var.sidecar_id}/self-signed-certificate*",
+      "arn:${local.aws_partition}:secretsmanager:${local.aws_region}:${local.aws_account_id}:secret:/cyral/sidecars/${var.sidecar_id}/ca-certificate*",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "self_signed_certificate" {
+  role       = aws_iam_role.self_signed_certificate.name
+  policy_arn = aws_iam_policy.self_signed_certificate.arn
 }
